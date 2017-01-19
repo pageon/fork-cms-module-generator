8daeff4ea5def1892ee3c65beff420d086213b2c
Jelmer Prins <jelmer@sumocoders.be>
2017-01-19 22:47:07 +0100

    The actual application

---
+++ ModuleGenerator.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use ModuleGenerator\Service\Update as UpdateService;
+use ModuleGenerator\CLI\Command\Update;
+use Symfony\Component\Console\Application;
+
+// Create new application
+$application = new Application();
+
+// Services
+$updateService = new UpdateService(__DIR__);
+
+// Add update command
+$application->add(new Update('update', $updateService));
+
+// Run the application
+$application->run();
