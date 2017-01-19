9a524d932eb751b46e33b9ff8bf5a7ab3f7131f9
Jelmer Prins <jelmer@sumocoders.be>
2017-01-20 00:59:46 +0100

    Create an app kernel for our application

---
+++ src/AppKernel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ModuleGenerator;
+
+use Matthias\SymfonyConsoleForm\Bundle\SymfonyConsoleFormBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+            new SymfonyConsoleFormBundle(),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config.yml');
+    }
+
+    public function getCacheDir()
+    {
+        return __DIR__.'/temp/cache';
+    }
+
+    public function getLogDir()
+    {
+        return __DIR__.'/temp/logs';
+    }
+
+    /**
+     * @throws \RuntimeException
+     *
+     * @return ContainerBuilder The compiled service container
+     */
+    protected function buildContainer()
+    {
+        $container = parent::buildContainer();
+
+        $container->setParameter('cwd', getcwd());
+
+        return $container;
+    }
+}
