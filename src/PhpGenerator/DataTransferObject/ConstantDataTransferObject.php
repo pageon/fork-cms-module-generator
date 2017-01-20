120fe6c41fdd2accbd5c945ab620712c7a0fbceb
Jelmer Prins <jelmer@sumocoders.be>
2017-01-20 21:52:00 +0100

    Data transfer object for constants

---
+++ src/PhpGenerator/DataTransferObject/ConstantDataTransferObject.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ModuleGenerator\PhpGenerator\DataTransferObject;
+
+class ConstantDataTransferObject extends MemberDataTransferObject
+{
+    /** @var mixed */
+    public $value;
+}
