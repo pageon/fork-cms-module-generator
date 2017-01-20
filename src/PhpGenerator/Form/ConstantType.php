68fdfe075e08a6349c25432167d90c47a7d7a873
Jelmer Prins <jelmer@sumocoders.be>
2017-01-20 21:52:34 +0100

    Form type for constants

---
+++ src/PhpGenerator/Form/ConstantType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ModuleGenerator\PhpGenerator\Form;
+
+use ModuleGenerator\PhpGenerator\DataTransferObject\ConstantDataTransferObject;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+final class ConstantType extends MemberType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->add(
+            'value',
+            $options['type_class_name'],
+            [
+                'label' => $options['value_label'],
+                'required' => true,
+                'constraints' => $options['value_constraints'],
+            ]
+        );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+        $resolver->setDefaults(
+            [
+                'data_class' => ConstantDataTransferObject::class,
+                'label' => 'Constant',
+                'type_class_name' => TextType::class,
+                'required' => true,
+                'value_label' => 'Value',
+                'value_constraints' => [
+                    new NotNull(),
+                ],
+            ]
+        );
+    }
+
+    public function getParent()
+    {
+        return MemberType::class;
+    }
+}
