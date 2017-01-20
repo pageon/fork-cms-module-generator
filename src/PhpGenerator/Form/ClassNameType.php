c9a2442a2625d3bad612c9b2714575d3b529ebeb
Jelmer Prins <jelmer@sumocoders.be>
2017-01-20 22:37:21 +0100

    Form type for class names

---
+++ src/PhpGenerator/Form/ClassNameType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ModuleGenerator\PhpGenerator\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
+
+final class ClassNameType extends TextType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+        $resolver->setDefaults(
+            [
+                'label' => 'Class name (i.e.: Status)',
+                'required' => true,
+                'constraints' => [
+                    new Regex(
+                        ['pattern' => '/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', 'message' => 'Invalid class name']
+                    ),
+                ],
+            ]
+        );
+    }
+
+    public function getParent()
+    {
+        return TextType::class;
+    }
+}
