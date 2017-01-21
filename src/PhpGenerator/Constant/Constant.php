09bf3ab9359d7a89c49138b5df124a47c2447a1a
Jelmer Prins <jelmer@sumocoders.be>
2017-01-21 02:25:50 +0100

    Add method to get the type of a constant value

--- src/PhpGenerator/Constant/Constant.php
+++ src/PhpGenerator/Constant/Constant.php
@@ -18,4 +18,15 @@ final class Constant extends NetteConstant
             ->setComment($constantDataTransferObject->comment)
             ->setVisibility($constantDataTransferObject->visibility);
     }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        // for historical reasons "double" is returned in case of a float,
+        // and not simply "float" from the function gettype.
+        // we make sure we return float instead of double :)
+        return str_replace('double', 'float', gettype($this->getValue()));
+    }
 }
