3032ebcaeb64b86ec59f18229c3ce2e896e61ae2
Jelmer Prins <jelmer@sumocoders.be>
2017-01-26 13:19:55 +0100

    Add a base class for module classes

---
+++ src/CLI/Service/Generate/GeneratableModuleClass.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ModuleGenerator\CLI\Service\Generate;
+
+use ModuleGenerator\PhpGenerator\ModuleName\ModuleName;
+
+abstract class GeneratableModuleClass extends GeneratableClass
+{
+    /** @var ModuleName */
+    private $moduleName;
+
+    public function __construct(ModuleName $moduleName)
+    {
+        $this->moduleName = $moduleName;
+    }
+
+    final public function getModuleName(): ModuleName
+    {
+        return $this->moduleName;
+    }
+
+    final protected function getFrontendModulePath(string $filePath = null): string
+    {
+        if ($filePath === null) {
+            return 'Frontend/Modules/' . $this->getModuleName();
+        }
+
+        return $this->getFrontendModulePath() . '/' . $filePath;
+    }
+
+    final protected function getBackendModulePath(string $filePath = null): string
+    {
+        if ($filePath === null) {
+            return 'Backend/Modules/' . $this->getModuleName();
+        }
+
+        return $this->getBackendModulePath() . '/' . $filePath;
+    }
+}
