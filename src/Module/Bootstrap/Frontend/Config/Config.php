22d2ff76e7694d383ea85ea86776cdf5bc773670
Jelmer Prins <jelmer@sumocoders.be>
2017-01-26 13:25:48 +0100

    Frontend config

---
+++ src/Module/Bootstrap/Frontend/Config/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ModuleGenerator\Module\Bootstrap\Frontend\Config;
+
+use ModuleGenerator\CLI\Service\Generate\GeneratableModuleClass;
+use ModuleGenerator\PhpGenerator\ClassName\ClassName;
+use ModuleGenerator\PhpGenerator\PhpNamespace\PhpNamespace;
+
+final class Config extends GeneratableModuleClass
+{
+    public function getClassName(): ClassName
+    {
+        return new ClassName('Config', new PhpNamespace('Frontend\\Modules\\' . $this->getModuleName()));
+    }
+}
