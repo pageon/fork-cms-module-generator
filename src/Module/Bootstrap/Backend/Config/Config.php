777aca69552926d0002956b145ef93592b75b437
Jelmer Prins <jelmer@sumocoders.be>
2017-01-26 13:27:13 +0100

    Backend config

---
+++ src/Module/Bootstrap/Backend/Config/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ModuleGenerator\Module\Bootstrap\Backend\Config;
+
+use ModuleGenerator\CLI\Service\Generate\GeneratableModuleClass;
+use ModuleGenerator\PhpGenerator\ClassName\ClassName;
+use ModuleGenerator\PhpGenerator\PhpNamespace\PhpNamespace;
+
+final class Config extends GeneratableModuleClass
+{
+    public function getClassName(): ClassName
+    {
+        return new ClassName('Config', new PhpNamespace('Backend\\Modules\\' . $this->getModuleName()));
+    }
+}
