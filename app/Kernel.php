e19caa76134f420a73ee1756e4cf9c9a17d08cc2
Jelmer Prins <jelmer@sumocoders.be>
2017-01-28 13:30:16 +0100

    fix container parameter not being set

--- app/Kernel.php
+++ app/Kernel.php
@@ -6,11 +6,21 @@ use Matthias\SymfonyConsoleForm\Bundle\SymfonyConsoleFormBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Kernel as SymfonyKernel;
 
 class Kernel extends SymfonyKernel
 {
-    public function registerBundles()
+    /** @var string */
+    private $vendorDirectory;
+
+    public function __construct(string $environment, string $debug, string $vendorDirectory)
+    {
+        parent::__construct($environment, $debug);
+        $this->vendorDirectory = $vendorDirectory;
+    }
+
+    public function registerBundles(): array
     {
         return [
             new FrameworkBundle(),
@@ -24,13 +34,22 @@ class Kernel extends SymfonyKernel
         $loader->load(__DIR__ . '/config.yml');
     }
 
-    public function getCacheDir()
+    public function getCacheDir(): string
     {
         return __DIR__ . '/cache';
     }
 
-    public function getLogDir()
+    public function getLogDir(): string
     {
         return __DIR__ . '/logs';
+    }
+
+    protected function buildContainer(): ContainerInterface
+    {
+        $container = parent::buildContainer();
+
+        $container->setParameter('vendor_directory', $this->vendorDirectory);
+
+        return $container;
     }
 }
