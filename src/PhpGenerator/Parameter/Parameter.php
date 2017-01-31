4eec10808d277823392940cc9b32f307c3aacc4e
Jelmer Prins <jelmer@sumocoders.be>
2017-01-31 23:24:36 +0100

    Parameter class

---
+++ src/PhpGenerator/Parameter/Parameter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ModuleGenerator\PhpGenerator\Parameter;
+
+use ModuleGenerator\PhpGenerator\ParameterType\DBALType;
+
+final class Parameter
+{
+    /** @var string */
+    private $name;
+
+    /** @var DBALType */
+    private $dbalType;
+
+    /** @var bool */
+    private $nullable;
+
+    public function __construct($name, DBALType $dbalType, $nullable)
+    {
+        $this->name = $name;
+        $this->dbalType = $dbalType;
+        $this->nullable = $nullable;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getDbalType(): DBALType
+    {
+        return $this->dbalType;
+    }
+
+    public function isNullable(): bool
+    {
+        return $this->nullable;
+    }
+
+    public static function fromDataTransferObject(ParameterDataTransferObject $parameterDataTransferObject): self
+    {
+        return new self(
+            $parameterDataTransferObject->name,
+            $parameterDataTransferObject->dbalType,
+            $parameterDataTransferObject->nullable
+        );
+    }
+}
