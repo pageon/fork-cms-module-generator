7426e53999aa4a17b141c54c508e8e22c659320d
Jelmer Prins <jelmer@sumocoders.be>
2017-02-03 00:56:08 +0100

    Make it more easy to get multiple pages out of the echo dumper

--- src/CLI/Service/Generate/EchoDumper.php
+++ src/CLI/Service/Generate/EchoDumper.php
@@ -6,8 +6,13 @@ final class EchoDumper implements Dumper
 {
     public function dump(string $filename, string $content)
     {
-        echo 'Filename: "' . $filename . '"' . PHP_EOL;
-        echo 'Content:' . PHP_EOL;
+        echo '########## START FILE ##########' . PHP_EOL;
+        echo '########## START FILENAME ##########' . PHP_EOL;
+        echo '# ' . $filename . ' #' . PHP_EOL;
+        echo '########## END FILENAME ##########' . PHP_EOL;
+        echo '########## START CONTENT ##########' . PHP_EOL;
         echo $content;
+        echo '########## END CONTENT ##########' . PHP_EOL;
+        echo '########## END FILE ##########' . PHP_EOL;
     }
 }
