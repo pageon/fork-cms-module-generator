96c368e63594d9c9314f59519b5b856c9f3e7f58
Bruno Vitorino <bruno@sumocoders.be>
2017-12-21 09:57:33 +0100

    Remove Settings.

--- src/CLI/Console/GenerateCommand.php
+++ src/CLI/Console/GenerateCommand.php
@@ -3,9 +3,7 @@
 namespace ModuleGenerator\CLI\Console;
 
 use ModuleGenerator\CLI\Service\Generate\Generate;
-use ModuleGenerator\CLI\Service\ModuleGenerator\Settings;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -14,9 +12,6 @@ abstract class GenerateCommand extends Command
 {
     /** @var Generate */
     protected $generateService;
-
-    /** @var Settings */
-    protected $settings;
 
     /** @var InputInterface */
     private $input;
@@ -33,27 +28,14 @@ abstract class GenerateCommand extends Command
             'php',
             InputArgument::OPTIONAL,
             'Target PHP version',
-            $this->getSettings()->get(Settings::DEFAULT_PHP_VERSION)
+            '7.1'
         );
     }
 
-    /**
-     * @param Generate $generateService
-     * @param Settings $settings
-     */
-    public function __construct(Generate $generateService, Settings $settings)
+    public function __construct(Generate $generateService)
     {
         $this->generateService = $generateService;
-        $this->settings = $settings;
         parent::__construct();
-    }
-
-    /**
-     * @return Settings
-     */
-    protected function getSettings()
-    {
-        return $this->settings;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -62,14 +44,6 @@ abstract class GenerateCommand extends Command
         self::$output = $output;
 
         $this->targetPhpVersion = (float) $this->input->getArgument('php');
-        if (!in_array($this->targetPhpVersion, $this->getSettings()->get(Settings::SUPPORTED_PHP_VERSIONS), true)) {
-            throw new InvalidArgumentException(
-                'This php version is not available as a target. The possible options are: ' . implode(
-                    ', ',
-                    $this->getSettings()->get(Settings::SUPPORTED_PHP_VERSIONS)
-                )
-            );
-        }
     }
 
     public function getTargetPhpVersion(): float
