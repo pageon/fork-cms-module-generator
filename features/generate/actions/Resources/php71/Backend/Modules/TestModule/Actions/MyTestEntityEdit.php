acb2dbb150a2c9ab3fceab265abacd2777a371f7
Bruno Vitorino <bruno@sumocoders.be>
2017-12-28 14:41:32 +0100

    Add usage of delete form type on edit action

--- features/generate/actions/Resources/php71/Backend/Modules/TestModule/Actions/MyTestEntityEdit.php
+++ features/generate/actions/Resources/php71/Backend/Modules/TestModule/Actions/MyTestEntityEdit.php
@@ -3,13 +3,29 @@
 namespace Backend\Modules\TestModule\Actions;
 
 use Backend\Core\Engine\Base\ActionEdit;
+use Backend\Form\Type\DeleteType;
+use Backend\Modules\TestModule\Domain\MyTestEntity\MyTestEntity;
 
 class MyTestEntityEdit extends ActionEdit
 {
     public function execute(): void
     {
         parent::execute();
+
+        $this->loadDeleteForm($myTestEntity); // Replace this with a real entity
+
         $this->parse();
         $this->display();
+    }
+
+    private function loadDeleteForm(MyTestEntity $myTestEntity): void
+    {
+        $deleteForm = $this->createForm(
+            DeleteType::class,
+            ['id' => $myTestEntity->getId()],
+            ['module' => $this->getModule()]
+        );
+
+        $this->template->assign('deleteForm', $deleteForm->createView());
     }
 }
