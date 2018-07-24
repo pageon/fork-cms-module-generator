90e894c1f520b67ff44216b46fd69f9e0d80ac54
Jelmer Prins <jelmer@sumocoders.be>
2018-07-24 14:40:57 +0200

    Use the modern Repository config

--- features/generate/domain/resources/php71/Backend/Modules/TestModule/Domain/MyTestEntity/MyTestEntityRepository.php
+++ features/generate/domain/resources/php71/Backend/Modules/TestModule/Domain/MyTestEntity/MyTestEntityRepository.php
@@ -2,10 +2,16 @@
 
 namespace Backend\Modules\TestModule\Domain\MyTestEntity;
 
-use Doctrine\ORM\EntityRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
-class MyTestEntityRepository extends EntityRepository
+class MyTestEntityRepository extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, MyTestEntity::class);
+    }
+
     public function add(MyTestEntity $myTestEntity): void
     {
         $this->getEntityManager()->persist($myTestEntity);
