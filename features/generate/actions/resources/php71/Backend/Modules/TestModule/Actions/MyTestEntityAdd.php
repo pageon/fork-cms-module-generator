8126979dbda5a75a5ed2015fe4484b590c9d7dbd
Jelmer Prins <jelmer@sumocoders.be>
2018-07-25 13:11:28 +0200

    Update the code of the Add action

--- features/generate/actions/resources/php71/Backend/Modules/TestModule/Actions/MyTestEntityAdd.php
+++ features/generate/actions/resources/php71/Backend/Modules/TestModule/Actions/MyTestEntityAdd.php
@@ -3,13 +3,64 @@
 namespace Backend\Modules\TestModule\Actions;
 
 use Backend\Core\Engine\Base\ActionAdd;
+use Backend\Core\Language\Locale;
+use Backend\Modules\TestModule\Domain\MyTestEntity\Command\CreateMyTestEntity;
+use Backend\Modules\TestModule\Domain\MyTestEntity\MyTestEntityType;
+use Backend\Core\Engine\Model as BackendModel;
+use Symfony\Component\Form\Form;
 
 class MyTestEntityAdd extends ActionAdd
 {
     public function execute(): void
     {
         parent::execute();
-        $this->parse();
-        $this->display();
+
+        $form = $this->getForm();
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            $this->template->assign('form', $form->createView());
+
+            $this->parse();
+            $this->display();
+
+            return;
+        }
+
+        $this->handleForm($form);
+    }
+
+    private function handleForm(Form $form): void
+    {
+        $createMyTestEntity = $form->getData();
+
+        $this->get('command_bus')->handle($createMyTestEntity);
+
+        $this->redirect(
+            $this->getBackLink(
+                [
+                    'report' => 'added',
+                    'var' => $createMyTestEntity->title,
+                ]
+            )
+        );
+    }
+
+    private function getForm(): Form
+    {
+        $form = $this->createForm(MyTestEntityType::class, new CreateMyTestEntity(Locale::workingLocale()));
+
+        $form->handleRequest($this->getRequest());
+
+        return $form;
+    }
+
+    private function getBackLink(array $parameters = []): string
+    {
+        return BackendModel::createUrlForAction(
+            'MyTestEntityIndex',
+            null,
+            null,
+            $parameters
+        );
     }
 }
