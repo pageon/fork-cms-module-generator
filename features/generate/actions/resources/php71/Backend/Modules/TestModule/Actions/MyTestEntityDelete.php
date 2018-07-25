1c69af6bd43f09e92f949eac458b1b3821b2b01c
Jelmer Prins <jelmer@sumocoders.be>
2018-07-25 13:26:42 +0200

    Update the code of the Delete action

--- features/generate/actions/resources/php71/Backend/Modules/TestModule/Actions/MyTestEntityDelete.php
+++ features/generate/actions/resources/php71/Backend/Modules/TestModule/Actions/MyTestEntityDelete.php
@@ -4,17 +4,25 @@ namespace Backend\Modules\TestModule\Actions;
 
 use Backend\Core\Engine\Base\ActionDelete;
 use Backend\Core\Engine\Model as BackendModel;
+use Backend\Form\Type\DeleteType;
+use Backend\Modules\TestModule\Domain\MyTestEntity\Command\DeleteMyTestEntity;
+use Backend\Modules\TestModule\Domain\MyTestEntity\MyTestEntity;
+use Backend\Modules\TestModule\Domain\MyTestEntity\MyTestEntityRepository;
 
 class MyTestEntityDelete extends ActionDelete
 {
     public function execute(): void
     {
-        parent::execute();
+        $myTestEntity = $this->getMyTestEntity();
+        if (!$myTestEntity instanceof MyTestEntity) {
+            $this->redirect(BackendModel::createUrlForAction('MyTestEntityIndex', null, null, ['error' => 'non-existing']));
 
-        // $this->redirect($this->getBackLink(['report' => 'deleted', 'var' => $this->record['title']]));
+            return;
+        }
 
-        $this->parse();
-        $this->display();
+        $this->get('command_bus')->handle(new DeleteMyTestEntity($myTestEntity));
+
+        $this->redirect($this->getBackLink(['report' => 'deleted', 'var' => $myTestEntity->getTitle()]));
     }
 
     private function getBackLink(array $parameters = []): string
@@ -25,5 +33,17 @@ class MyTestEntityDelete extends ActionDelete
             null,
             $parameters
         );
+    }
+
+    private function getMyTestEntity(): MyTestEntity
+    {
+        $deleteForm = $this->createForm(DeleteType::class, null, ['module' => $this->getModule()]);
+        $deleteForm->handleRequest($this->getRequest());
+
+        if (!$deleteForm->isSubmitted() || !$deleteForm->isValid()) {
+            return null;
+        }
+
+        $this->get(MyTestEntityRepository::class)->find($deleteForm->getData()['id']);
     }
 }
