b67cbd7330cafb07abfd5d4f749b3320128cb5f6
Jelmer Prins <jelmer@sumocoders.be>
2018-07-26 10:10:09 +0200

    Generate Edit action

--- features/generate/actions/resources/php71/Backend/Modules/TestModule/Actions/MyTestEntityEdit.php
+++ features/generate/actions/resources/php71/Backend/Modules/TestModule/Actions/MyTestEntityEdit.php
@@ -5,6 +5,11 @@ namespace Backend\Modules\TestModule\Actions;
 use Backend\Core\Engine\Base\ActionEdit;
 use Backend\Form\Type\DeleteType;
 use Backend\Modules\TestModule\Domain\MyTestEntity\MyTestEntity;
+use Backend\Modules\TestModule\Domain\MyTestEntity\Command\UpdateMyTestEntity;
+use Backend\Core\Engine\Model as BackendModel;
+use Backend\Modules\TestModule\Domain\MyTestEntity\MyTestEntityRepository;
+use Backend\Modules\TestModule\Domain\MyTestEntity\MyTestEntityType;
+use Symfony\Component\Form\Form;
 
 class MyTestEntityEdit extends ActionEdit
 {
@@ -12,10 +17,27 @@ class MyTestEntityEdit extends ActionEdit
     {
         parent::execute();
 
-        $this->loadDeleteForm($myTestEntity); // Replace this with a real entity
+        $myTestEntity = $this->getMyTestEntity();
 
-        $this->parse();
-        $this->display();
+        if (!$myTestEntity instanceof MyTestEntity) {
+            $this->redirect($this->getBackLink(['error' => 'non-existing']));
+        }
+
+        $this->loadDeleteForm($myTestEntity);
+
+        $form = $this->getForm($myTestEntity);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            $this->template->assign('form', $form->createView());
+            $this->template->assign('myTestEntity', $myTestEntity);
+
+            $this->parse();
+            $this->display();
+
+            return;
+        }
+
+        $this->handleForm($form);
     }
 
     private function loadDeleteForm(MyTestEntity $myTestEntity): void
@@ -23,9 +45,52 @@ class MyTestEntityEdit extends ActionEdit
         $deleteForm = $this->createForm(
             DeleteType::class,
             ['id' => $myTestEntity->getId()],
-            ['module' => $this->getModule()]
+            [
+                'module' => $this->getModule(),
+                'action' => 'MyTestEntityDelete',
+            ]
         );
 
         $this->template->assign('deleteForm', $deleteForm->createView());
+    }
+
+    private function getForm(MyTestEntity $myTestEntity): Form
+    {
+        $form = $this->createForm(MyTestEntityType::class, new UpdateMyTestEntity($myTestEntity));
+
+        $form->handleRequest($this->getRequest());
+
+        return $form;
+    }
+
+    private function handleForm(Form $form): void
+    {
+        $updateMyTestEntity = $form->getData();
+
+        $this->get('command_bus')->handle($updateMyTestEntity);
+
+        $this->redirect(
+            $this->getBackLink(
+                [
+                    'report' => 'edited',
+                    'var' => $updateMyTestEntity->title,
+                ]
+            )
+        );
+    }
+
+    private function getBackLink(array $parameters = []): string
+    {
+        return BackendModel::createUrlForAction(
+            'MyTestEntityIndex',
+            null,
+            null,
+            $parameters
+        );
+    }
+
+    private function getMyTestEntity(): ?MyTestEntity
+    {
+        return $this->get(MyTestEntityRepository::class)->find($this->getRequest()->query->getInt('id'));
     }
 }
