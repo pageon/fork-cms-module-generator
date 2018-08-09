9c807d4b7d5305c7ec7470d1875e72ad7d94b6c2
Jelmer Prins <jelmer@sumocoders.be>
2018-08-09 07:40:05 +0200

    Fix value object dbal type

--- features/generate/domain/resources/php71/Standalone/MyValueObjectDBALType.php
+++ features/generate/domain/resources/php71/Standalone/MyValueObjectDBALType.php
@@ -3,21 +3,21 @@
 namespace Standalone;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\StringType;
 
-final class MyValueObjectDBALType extends Type
+final class MyValueObjectDBALType extends StringType
 {
     public function getName(): string
     {
         return 'myvalueobject';
     }
 
-    public function convertToPHPValue(string $myValueObjectDBALType, AbstractPlatform $platform): MyValueObject
+    public function convertToPHPValue($myValueObjectDBALType, AbstractPlatform $platform): MyValueObject
     {
         return new MyValueObject($myValueObjectDBALType);
     }
 
-    public function convertToDatabaseValue(MyValueObject $myValueObjectDBALType, AbstractPlatform $platform): string
+    public function convertToDatabaseValue($myValueObjectDBALType, AbstractPlatform $platform): string
     {
         return $myValueObjectDBALType->getValue();
     }
